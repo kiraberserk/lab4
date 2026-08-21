e82b03b21955d38f29b1ea5b76b3603f071ca366
Kira Berserk <juanc.castilloc@gmail.com>
2026-08-20 21:43:59 -0400

    Fixing the outputs bug - 5

--- envs/devel/main.tf
+++ envs/devel/main.tf
@@ -27,3 +27,12 @@ module "site" {
 output "cloudfront_domain" {
   value = module.site.cloudfront_domain
 }
+output "bucket_name" {
+  value = module.site.bucket_name
+}
+output "logs_bucket" {
+  value module.logs.logs_bucket
+}
+output "distribution_id" {
+  value = module.site.distribution_id
+}
